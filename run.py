b62de5a4d35ae66d286b8396390326fe27cf4224
MagnusNilssonn <mankan.nilsson@gmail.com>
2023-01-18 14:04:05 +0000

    create function for setting up playfield

--- run.py
+++ run.py
@@ -7,7 +7,13 @@ Guess_Playfield = [[' '] * 7 for x in range(7)]
 let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6 }
 
 def print_board(board):
-    pass
+    print('  A B C D E F G')
+    print(' ****************')
+    row_num = 1
+    for row in board:
+        print("%d|%s|" % (row_num, "|".join(row )))
+        row_num += 1
+        
 
 
 def get_ship_location():
