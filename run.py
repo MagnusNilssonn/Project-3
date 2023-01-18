cce7adfef402a6f7bd90bfa62deead195351fd1b
MagnusNilssonn <mankan.nilsson@gmail.com>
2023-01-18 13:44:42 +0000

    setup playfield and creating functions that will be used

--- run.py
+++ run.py
@@ -1,4 +1,25 @@
 # 'O' indicates a miss
 # 'X' indicates a hit
 
+Hidden_Playfield = [[' '] * 7 for x in range(7)]
+Guess_Playfield = [[' '] * 7 for x in range(7)]
+
+let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6 }
+
+def print_board(board):
+    pass
+
+
+def get_ship_location():
+    pass
+
+
+def create_ships(board):
+    pass
+
+
+def count_hit_ships(board):
+    pass
+
+
 
